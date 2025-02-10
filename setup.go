9acddf305a5c9a661296115537fa69297cc6e252
underfake <67966685+underfake@users.noreply.github.com>
2025-02-10 13:25:06 +0100

    add init function

--- setup.go
+++ setup.go
@@ -2,9 +2,11 @@ package neoteqts4via6
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"strings"
 
+	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/plugin"
 	"github.com/miekg/dns"
 )
@@ -12,6 +14,22 @@ import (
 // NeoteqTS4via6 ist das CoreDNS-Plugin
 type NeoteqTS4via6 struct {
 	Next plugin.Handler
+}
+
+// init registriert das Plugin bei CoreDNS
+func init() {
+	plugin.Register("neoteqts4via6", setup)
+}
+
+// setup konfiguriert das Plugin in CoreDNS
+func setup(c *caddy.Controller) error {
+	p := NeoteqTS4via6{}
+	c.OnStartup(func() error {
+		fmt.Println("NeoteqTS4via6 Plugin geladen!")
+		return nil
+	})
+	plugin.NextOrFailure(p.Name(), p.Next, context.Background(), nil, nil)
+	return nil
 }
 
 // ServeDNS verarbeitet DNS-Anfragen für AAAA-Records
@@ -27,7 +45,7 @@ func (p NeoteqTS4via6) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dn
 			msg.Authoritative = true
 
 			rr := &dns.AAAA{
-				Hdr: dns.RR_Header{Name: qName, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 60},
+				Hdr:  dns.RR_Header{Name: qName, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 60},
 				AAAA: net.ParseIP(ipv6),
 			}
 			msg.Answer = append(msg.Answer, rr)
